16429b71ce11dbf27c997dae87d81984c6a53aaa
WEI Zhanjun <42709269+SunSetPilot@users.noreply.github.com>
2023-09-11 20:49:59 +0800

    Update mcr.cpp

--- mcr.cpp
+++ mcr.cpp
@@ -1,51 +1,61 @@
 #include <iostream>
-use namespace std;
+using namespace std;
+
 
 bool isWin(char game[3][3]){
-	bool win = false;
-	if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-	if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true;
-	if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true;
-	// column
-	if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-	if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O')) win = true;
-	if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true;
-	// diagonal
-	if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-	if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true;
-	return win;
+    bool win = false;
+    for (int i = 0; i < 3; i++) {
+        if ((game[i][0] != ' ' && game[i][0] == game[i][1] && game[i][1] == game[i][2]) ||
+        (game[0][i] != ' ' && game[0][i] == game[1][i] && game[1][i] == game[2][i])) win = true;
+    }
+    // diagonal
+    if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] != ' ')) win = true;
+    if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] != ' ')) win = true;
+    return win;
 }
 
 int main(){
-	int i, j;
-	char game[3][3] = {' '}; // Tic-tac-toe
-	char player1 = 'X';
-	char player2 = 'O';
-	bool turn = true; // false for player 1's turn, true for player 2's turn. Player 1 first.
-	cout << "X = Player 1" << endl << "O = Player 2" << endl;
-       for (int n=0; n<9; n++){
-			turn = !turn;  // use the not-operator to change true to false or false to true.
-		    if (turn == false)
-				cout << "Player 1: ";
-			else
-				cout << "Player 2: ";
-			cout << "Which cell to mark? i:[1..3], j:[1..3]: "; 
-			cin >> i >> j;
-			if (turn == false)
-			   game[i][j] = 'X';
-			else 
-			   game[i][j] = 'O';
-			if (isWin(game)){
-				cout << "Win!" << endl;
-				break; // need to terminate the problem
-			}
-		}
-	if (i==3) // all celles with i=0 to 2 have been inputted above but no winner yet
-	   cout << "Tie!" << endl;
+    int i, j;
+    char game[3][3] = {{' ', ' ' , ' '},
+                       {' ', ' ' , ' '},
+                       {' ', ' ' , ' '} }; // Tic-tac-toe
+    bool player2_turn = true; // false for player 1's player2_turn, true for player 2's player2_turn. Player 1 first.
+    int n;
+    const char player1 = 'X';
+    const char player2 = 'O';
+    cout << player1 << " = Player 1" << endl << player2 <<" = Player 2" << endl;
+    for (n = 0; n < 9; n++){
+        char character;
+        player2_turn = !player2_turn;  // use the not-operator to change true to false or false to true.
+        if (!player2_turn) {
+            cout << "Player 1: ";
+            character = player1;
+        }
+        else {
+            cout << "Player 2: ";
+            character = player2;
+        }
+        cout << "Which cell to mark? i:[1..3], j:[1..3]: ";
+        cin >> i >> j;
+        while ((i < 1 || i > 3 || j < 1 || j > 3) || (game[i - 1][j - 1] == 'X' or game[i - 1][j - 1] == 'O')) {
+            cout << "Invalid input! Try again." << endl;
+            cout << "Which cell to mark? i:[1..3], j:[1..3]: ";
+            cin >> i >> j;
+        }
+        game[i - 1][j - 1] = character;
+        if (isWin(game)){
+            cout << "Win!" << endl;
+            break; // need to terminate the problem
+        }
+    }
+    if (n == 9) // all cells with i=1..3 and j=1..3 are filled in without a winner
+        cout << "Tie!" << endl;
 
-	// show the game to console
-	cout << game[0][0] << " " << game[0][1] << " " << game[0][2] << endl;
-	cout << game[1][0] << " " << game[1][1] << " " << game[1][2] << endl;
-	cout << game[2][0] << " " << game[2][1] << " " << game[2][2] << endl;
-	return 0;
+    // show the game to console
+    for (auto & row : game) {
+        for (int col = 0; col < 2; col++)
+            cout << row[col] << " ";
+        cout << endl;
+    }
+    return 0;
 }
